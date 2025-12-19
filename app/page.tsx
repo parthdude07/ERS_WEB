4631419c06c1bf887fe1c6796fedb1d8f70e8dac
Mohit Jaint <imohitjaint@gmail.com>
2025-12-19 16:49:27 +0530

    style : change hero robot positioning  for small devices

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
         <div className="relative w-full grid min-h-[80vh] grid-cols-1 md:grid-cols-2 gap-8 px-6 py-20 items-center">
 
           {/* LEFT: ROBOT */}
-          <div className="relative flex items-center justify-center overflow-visible">
+          <div className="relative flex items-center justify-center overflow-visible order-2 md:order-1">
 
             {/* Glow layer */}
             <div
@@ -30,7 +30,8 @@ export default async function Home() {
                 z-0
               "
             >
-              <div className="w-90 h-70 bg-ers-yellow/20 rounded-full blur-3xl -translate-x-20" />
+            <div className="w-72 h-56 md:w-90 md:h-70 bg-ers-yellow/20 rounded-full blur-3xl -translate-x-20 md:-translate-x-20" />
+
             </div>
 
             {/* Canvas */}
@@ -42,7 +43,8 @@ export default async function Home() {
 
 
           {/* RIGHT: TEXT */}
-          <div className="flex flex-col justify-center text-left md:pl-12">
+          <div className="flex flex-col justify-center text-left md:pl-12 order-1 md:order-2">
+
             <h1 className="font-tech text-5xl md:text-7xl font-bold tracking-[0.12em] text-transparent bg-clip-text bg-gradient-to-r from-ers-yellow via-white to-ers-yellow drop-shadow-[0_0_18px_rgba(244,196,48,0.45)]">
               ERS CLUB
             </h1>
