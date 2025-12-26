976de049b010f49543976020889b121395175842
PARTH DUDHE <parthdudhe06@gmail.com>
2025-12-26 11:40:59 +0530

    hardcoded sanity variables

--- dist/static/sanity-Bhuqtyic.js
+++
@@ -1,1 +0,0 @@
-import{r as t,s as e}from"./sanity.config-BFymuY6K.js";t(document.getElementById("sanity"),e,{reactStrictMode:!1,basePath:"/"});
---
+++ dist/static/sanity-C2ZjAzsz.js
@@ -0,0 +1,1 @@
+import{r as t,s as e}from"./sanity.config-Db4rYjuU.js";t(document.getElementById("sanity"),e,{reactStrictMode:!1,basePath:"/"});
